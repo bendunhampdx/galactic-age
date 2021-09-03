143f20a98f8ff2143517f1ce2a4222763e2986ed
bendunhampdx <bendunhampdx@gmail.com>
2021-09-03 14:51:53 -0700

    put old tests/functions for determining age in just in case they were nesessary for grading purposes

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,6 +2,28 @@ import { GalacticAge } from './../src/js/galactic.js';
 
 describe('GalacticAge', () => {
 
+  test('should correctly determine age on Earth', () => {
+    const earth = new GalacticAge(34, 80);
+    expect(earth.getEarthAge()).toBeCloseTo(34);
+  })
+
+  test('should correctly determine users age on Mercury', () => {
+    const mercury = new GalacticAge(34, 80);
+    expect(mercury.getMercuryAge()).toBeCloseTo(8.16);
+  })
+  test('should correctly determine users age on venus', () => {
+    const venus = new GalacticAge(34, 80);
+    expect(venus.getVenusAge()).toBeCloseTo(21.08);
+  })
+  test('should correctly determine users age on mars', () => {
+    const mars = new GalacticAge(34, 80);
+    expect(mars.getMarsAge()).toBeCloseTo(63.92);
+  })
+  test('should correctly determine users age on jupiter', () => {
+    const jupiter = new GalacticAge(34, 80);
+    expect(jupiter.getJupiterAge()).toBeCloseTo(403.24);
+  })
+
   test('should correctly determine life left on earth', () => {
     const earth = new GalacticAge(34, 80);
     expect(earth.getEarthLifeLeft()).toBeCloseTo(46);
--- src/js/galactic.js
+++ src/js/galactic.js
@@ -4,6 +4,27 @@ export class GalacticAge {
     this.lifeExpectency = lifeExpectency;
   }
 
+  getEarthAge() {
+    this.age = this.age *1;
+    return this.age
+  };
+  getMercuryAge() {
+    this.age = this.age * .24;
+    return this.age
+  };
+  getVenusAge() {
+    this.age = this.age * .62;
+    return this.age
+  };
+  getMarsAge() {
+    this.age = this.age * 1.88;
+    return this.age
+  };
+  getJupiterAge() {
+    this.age = this.age * 11.86;
+    return this.age
+  };
+
   getEarthLifeLeft() {
     this.age = this.age * 1;
     this.lifeExpectency = this.lifeExpectency * 1;
