790bcd93a8662bb069ccd31be5c57a1d9abb61ed
bendunhampdx <bendunhampdx@gmail.com>
2021-09-03 08:54:28 -0700

    recieve good fail on second test for calculating age on venus

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,6 +1,14 @@
 import { GalacticAge } from './../src/js/galactic.js';
 
+describe('GalacticAge', () => {
+
   test('should correctly determine users age on Mercury', () => {
     const mercuryAge = new GalacticAge(34);
     expect(mercuryAge.getAge()).toBeCloseTo(8.16)
   })
+
+  test('should correctly determine users age on Venus', () => {
+    const venusAge = new GalacticAge(34);
+    expect(venusAge.getAge()).toBeCloseTo()
+  })
+})
